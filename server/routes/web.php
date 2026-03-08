1dadbc0f55bc143db812baefa022d739a4208ba0
miziauhqrm <109470895+Corazons@users.noreply.github.com>
2026-03-08 14:57:48 +0700

    remove unnecessary statement

--- server/routes/web.php
+++ server/routes/web.php
@@ -5,33 +5,16 @@ use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Redis;
 
-$employee = [
-    "E001" => ["name" => "Alice Smith", "role" => "Developer"],
-    "E002" => ["name" => "Bob Jones", "role" => "Designer"],
-    "E003" => ["name" => "Charlie Brown", "role" => "Manager"]
-];
-
+//cek laravel bekerja
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/user/{id?}', function (?string $id = null) use ($employee) {
-    if (!$id) {
-        return response()->json($employee);
-    }
-
-    if (!isset($employee[$id])) {
-        return response()->json([
-            "message" => "Employee tidak ditemukan"
-        ], 404);
-    }
-
-    return response()->json($employee[$id]);
-});
-
+//cek redis sudah terkoneksi
 Route::get('/redis-test', function () {
     Redis::set('check', 'Redis Connect');
     return Redis::get('check');
 });
 
+//mengambil data dari redis
 Route::get('/photos', [ApiController::class, 'index']);
